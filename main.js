7611de06a63638661a39a2126eea8ed0a38c632c
Francesco <francesco.ferruccio88@gmail.com>
2020-04-16 12:04:41 +0200

    ex 2

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// ex 1
 // Creare un oggetto palla che abbia le seguenti proprietà. Nome = palla
 // Peso = 10
 // Attraverso un prompt dare la possibilità all’utente di modificare il peso della palla
@@ -6,8 +7,28 @@ var palla = {
   "nome": "palla",
   "peso": 10
 };
-console.log("oggetto iniziale", palla);
 
-palla.peso = parseInt(prompt("Inserisci il nuovo peso"));
+// console.log("oggetto iniziale", palla);
+//
+// palla.peso = parseInt(prompt("Inserisci il nuovo peso"));
+//
+// console.log("oggetto dopo prompt", palla);
 
-console.log("oggetto dopo prompt", palla);
+// ex 2
+// inpupt utente per cambiare peso lo prendiamo da input ()invece che prompt
+// e poi stampiamo l’oggetto in pagina, quindi non grezzo tramite log in console, ma output “carino” in pagina
+
+
+$("button").click(
+  function () {
+    var input = $("#inputPeso").val();
+    palla.peso = input;
+
+    $("#output").html("");
+
+    for (var key in palla) {
+      $("#output").append("<p>" + key + " " + palla[key] + "</p>");
+    }
+
+  }
+)
